dbde7e70e3f0e971a838d7b9cf19f22c9653eccb
Andries Niemandt <andries.niemandt91@gmail.com>
2022-04-27 21:51:42 +0200

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Manufacturer.create!(name: "Boeing", description: "Boeing, headquartered in Chicago, is the largest of the commercial aircraft manufacturers.")
+Manufacturer.create!(name: "Airbus", description: "Airbus is the other major manufacturer of large commercial aircraft along with Boeing. Notably, its headquarters reside in Leiden in the Netherlands.")
